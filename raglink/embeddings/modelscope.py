c5634bf0ae88508655f6f21fd5e2895bb0be547f
草根之明 <3907721@qq.com>
2025-06-07 15:13:18 +0800

    Update modelscope.py

--- raglink/embeddings/modelscope.py
+++ raglink/embeddings/modelscope.py
@@ -7,13 +7,18 @@
 # @File    : modelscope
 # @desc    :
 
+from modelscope.models import Model
 from raglink.embeddings.base import EmbeddingBase
 from langchain_community.embeddings import ModelScopeEmbeddings
 
 
 class ModelScopeEmbedding(EmbeddingBase):
-    def __init__(self, model_name):
-        self.embeddings = ModelScopeEmbeddings(model_id=model_name)
+    def __init__(self, model_id=None, model_path=None):
+        if model_id is not None:
+            self.embeddings = ModelScopeEmbeddings(model_id=model_id)
+        if model_path is not None:
+            model = Model.from_pretrained(model_path)
+            self.embeddings = ModelScopeEmbeddings(model=model)
 
     def embed(self, text):
         return self.embeddings.embed_query(text)
